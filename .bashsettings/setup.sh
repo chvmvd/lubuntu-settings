9fe9a21eb03669dd76b54cd5ce2f4101496a2895
chvmvd <chvmvd@users.noreply.github.com>
2022-10-28 17:40:32 +0900

    Dockerのインストール方法を更新

--- .bashsettings/setup.sh
+++ .bashsettings/setup.sh
@@ -59,18 +59,23 @@ fi
 
 # Docker
 if ! type docker > /dev/null 2>&1;then
-  sudo apt -y install gnome-terminal
-  sudo apt update
-  sudo apt install ca-certificates curl gnupg lsb-release
+  sudo apt-get update
+  sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
   sudo mkdir -p /etc/apt/keyrings
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
   echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  curl -o dockerLatest.deb https://desktop.docker.com/linux/main/amd64/docker-desktop-4.11.0-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64 \
-  sudo apt update
-  sudo apt install ./dockerLatest.deb
-  rm dockerLatest.deb
+  sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+  sudo groupadd docker
+  sudo usermod -aG docker $USER
+  newgrp docker
 fi
 
 # sudo ln -s /usr/share/doc/git/contrib/diff-highlight/diff-highlight /usr/local/bin/diff-highlight
